7baec6a87db08fde0f544a6cc8f131f911588a65
Glen Pierce <glen.piercev@gmail.com>
2019-03-10 18:23:19 -0700

    Better cards - in progress.

---
+++ data/macros.js
@@ -0,0 +1,57 @@
+MACROS =
+    { "%air%":                                      "<img class='element' style='height:20px; width:20px;' src='icons/air.svg'>"
+        , "%any%":                                      "<img class='element' style='height:20px; width:20px;' src='icons/any_element.svg'>"
+        , "%aoe-4-with-black%":                         "<img class='aoe h2' style='height:20px; width:20px;' src='icons/aoe-4-with-black.svg'>"
+        , "%aoe-circle%":                               "<div class='collapse small'><img class='aoe h3' style='height:20px; width:20px;' src='icons/aoe-circle.svg'></div>"
+        , "%aoe-circle-with-middle-black%":             "<div class='collapse small'><img class='aoe h3' style='height:20px; width:20px;' src='icons/aoe-circle-with-middle-black.svg'></div>"
+        , "%aoe-circle-with-side-black%":               "<img class='aoe h3' style='height:20px; width:20px;' src='icons/aoe-circle-with-side-black.svg'>"
+        , "%aoe-line-3-with-black%":                    "<div class='collapse'><img class='aoe h1 rotated' style='height:20px; width:20px;' src='icons/aoe-line-3-with-black.svg'></div>"
+        , "%aoe-line-4-with-black%":                    "<div class='collapse'><img class='aoe h1 rotated' style='height:20px; width:20px;' src='icons/aoe-line-4-with-black.svg'></div>"
+        , "%aoe-line-6-with-black%":                    "<img class='aoe h6 right-aligned' style='height:20px; width:20px;' src='icons/aoe-line-6-with-black.svg'></div>"
+        , "%aoe-triangle-2-side%":                      "<div class='collapse'><img class='aoe h2' style='height:20px; width:20px;' src='icons/aoe-triangle-2-side.svg'></div>"
+        , "%aoe-triangle-2-side-with-black%":           "<div class='collapse'><img class='aoe h2' style='height:20px; width:20px;' src='icons/aoe-triangle-2-side-with-black.svg'></div>"
+        , "%aoe-triangle-3-side-with-corner-black%":    "<div class='collapse'><img class='aoe h3' style='height:20px; width:20px;' src='icons/aoe-triangle-3-side-with-corner-black.svg'></div>"
+        , "%attack%":                                   "<span class='nobr'>Attack <img class='icon' style='height:20px; width:20px;' src='icons/attack.svg'></span>"
+        , "%bless%":                                    "<span class='nobr'>BLESS <img class='icon' style='height:20px; width:20px;' src='icons/bless.svg'></span>"
+        , "%boss-aoe-elder-drake-sp1%":                 "<div class='collapse'><img class='aoe h3' style='height:20px; width:20px;' src='icons/elderDrake.special1Area.svg'></div>"
+        , "%boss-aoe-inox-bodyguard-sp1%":              "<div class='collapse'><img class='aoe h3' style='height:20px; width:20px;' src='icons/inoxBodyguard.special1Area.svg'></div>"
+        , "%boss-aoe-sightless-eye-sp1%":               "<div class='collapse'><img class='aoe h3' style='height:20px; width:20px;' src='icons/sightlessEye.special1Area.svg'></div>"
+        , "%boss-aoe-sightless-eye-sp2%":               "<div class='collapse'><img class='aoe h3' style='height:20px; width:20px;' src='icons/sightlessEye.special2Area.svg'></div>"
+        , "%curse%":                                    "<span class='nobr'>CURSE <img class='icon' style='height:20px; width:20px;' src='icons/curse.svg'></span>"
+        , "%dark%":                                     "<img class='element' style='height:20px; width:20px;' src='icons/dark.svg'>"
+        , "%disarm%":                                   "<span class='nobr'>DISARM <img class='icon' style='height:20px; width:20px;' src='icons/disarm.svg'></span>"
+        , "%earth%":                                    "<img class='element' style='height:20px; width:20px;' src='icons/earth.svg'>"
+        , "%fire%":                                     "<img class='element' style='height:20px; width:20px;' src='icons/fire.svg'>"
+        , "%heal%":                                     "<span class='nobr'>Heal <img class='icon' style='height:20px; width:20px;' src='icons/heal.svg'></span>"
+        , "%ice%":                                      "<img class='element' style='height:20px; width:20px;' src='icons/ice.svg'>"
+        , "%immobilize%":                               "<span class='nobr'>IMMOBILIZE <img class='icon' style='height:20px; width:20px;' src='icons/immobilize.svg'></span>"
+        , "%invisible%":                                "<span class='nobr'>INVISIBLE <img class='icon' style='height:20px; width:20px;' src='icons/invisibility.svg'></span>"
+        , "%jump%":                                     "<span class='nobr'>Jump <img class='icon' style='height:20px; width:20px;' src='icons/jump.svg'></span>"
+        , "%light%":                                    "<img class='element' style='height:20px; width:20px;' src='icons/light.svg'>"
+        , "%loot%":                                     "<span class='nobr'>Loot <img class='icon' style='height:20px; width:20px;' src='icons/loot.svg'></span>"
+        , "%move%":                                     "<span class='nobr'>Move <img class='icon' style='height:20px; width:20px;' src='icons/move.svg'></span>"
+        , "%muddle%":                                   "<span class='nobr'>MUDDLE <img class='icon' style='height:20px; width:20px;' src='icons/muddle.svg'></span>"
+        , "%pierce%":                                   "<span class='nobr'>PIERCE <img class='icon' style='height:20px; width:20px;' src='icons/pierce.svg'></span>"
+        , "%poison%":                                   "<span class='nobr'>POISON <img class='icon' style='height:20px; width:20px;' src='icons/poison.svg'></span>"
+        , "%pull%":                                     "<span class='nobr'>PULL <img class='mirrored icon' style='height:20px; width:20px;' src='icons/push.svg'></span>"
+        , "%push%":                                     "<span class='nobr'>PUSH <img class='icon' style='height:20px; width:20px;' src='icons/push.svg'></span>"
+        , "%range%":                                    "<span class='nobr'>Range <img class='icon' style='height:20px; width:20px;' src='icons/range.svg'></span>"
+        , "%retaliate%":                                "<span class='nobr'>Retaliate <img class='icon' style='height:20px; width:20px;' src='icons/retaliate.svg'></span>"
+        , "%shield%":                                   "<span class='nobr'>Shield <img class='icon' style='height:20px; width:20px;' src='icons/shield.svg'></span>"
+        , "%flying%":                                   "<span class='nobr'><img class='icon' style='height:20px; width:20px;' src='icons/fly.svg'></span>"
+        , "%strengthen%":                               "<span class='nobr'>STRENGTHEN <img class='icon' style='height:20px; width:20px;' src='icons/strengthen.svg'></span>"
+        , "%stun%":                                     "<span class='nobr'>STUN <img class='icon' style='height:20px; width:20px;' src='icons/stun.svg'></span>"
+        , "%target%":                                   "<span class='nobr'>Target <img class='icon' style='height:20px; width:20px;' src='icons/target.svg'></span>"
+        , "%use_element%":                              "<img class='element overlay' style='height:20px; width:20px;' src='icons/use_element.svg'>"
+        , "%wound%":                                    "<span class='nobr'>WOUND <img class='icon' style='height:20px; width:20px;' src='icons/wound.svg'></span>"
+    };
+
+function expandMacro(macro)
+{
+    let key = macro.toLowerCase();
+    if (key in MACROS) {
+        return MACROS[key];
+    } else {
+        return macro;
+    }
+}
